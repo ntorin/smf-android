c513fa7ee37b3c3d20bd42bc93a38c2e795299ec
Ntori Nyamekye <xtoribear@gmail.com>
2017-01-11 06:52:41 -0800

    added edit profile

---
+++ app/src/main/java/com/inami/smf/personal/EditProfile.java
@@ -0,0 +1,60 @@
+package com.inami.smf.personal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.GridView;
+import android.widget.ImageButton;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.inami.smf.R;
+
+public class EditProfile extends AppCompatActivity {
+
+    private DatabaseReference mDatabase;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        ImageButton editAvatar = (ImageButton) findViewById(R.id.edit_profile_picture);
+        final GridView profilePictureSelection = (GridView) findViewById(R.id.profile_picture_selection);
+
+        editAvatar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(profilePictureSelection.getVisibility() == View.VISIBLE){
+                    profilePictureSelection.setVisibility(View.GONE);
+                }else{
+                    profilePictureSelection.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_edit_profile, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id){
+            case R.id.action_edit_profile_complete:
+                finish();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+}
