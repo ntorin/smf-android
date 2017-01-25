d58332194dba2a8f5d972ef1d30192bfe580c0c6
ntorin <xtoribear@gmail.com>
2017-01-25 10:08:58 -0800

    01/25/17 progress

--- app/src/main/java/com/inami/smf/bbs/BBSFragment.java
+++ app/src/main/java/com/inami/smf/bbs/BBSFragment.java
@@ -96,7 +96,7 @@ public class BBSFragment extends Fragment {
                 }
                 mThreadList.add(threadPreview);
                 mItemAdapter.notifyDataSetChanged();
-
+ 
                 Log.d("onChildAdded", "" + dataSnapshot.getValue());
             }
 
